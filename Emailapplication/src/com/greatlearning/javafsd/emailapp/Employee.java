3e1d4b52ada77d0f0ca73d334a456da81ba30d62
murugan <127122582+muruganm29@users.noreply.github.com>
2023-03-18 17:30:07 +0530

    Add files via upload
    Problem statement for the Lab OOPs assessment- email application

---
+++ Emailapplication/src/com/greatlearning/javafsd/emailapp/Employee.java
@@ -0,0 +1,52 @@
+package com.greatlearning.javafsd.emailapp;
+
+public class Employee {
+    private String firstname;
+    private String lastname;
+    private String department;
+    
+    private String emailAddress;
+    private String password;
+    
+	public Employee(String firstname, String lastname, String department) {
+		
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.department = department;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public String getDepartment() {
+		return department;
+	}
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+	public void setEmailAddress(String emailAddress) {
+		
+		
+	}
+	public Object getEmailAddress() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	public Object getPassword() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	public void setPassword(String password2) {
+		// TODO Auto-generated method stub
+		
+	}
+    
+}
